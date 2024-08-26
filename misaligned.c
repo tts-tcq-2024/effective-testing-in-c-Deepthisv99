a2df4e1343f196a60cb579b88bdc0d947c2e5598
Deepthisv99 <deepthivenki1@gmail.com>
2024-08-26 18:28:11 +0530

    Update misaligned.c

--- misaligned.c
+++ misaligned.c
@@ -1,21 +1,30 @@
 #include <stdio.h>
 #include <assert.h>
+#include <string.h>
 
-int printColorMap() {
+// Function to generate color mappings
+int printColorMap(char *outputBuffer, size_t bufferSize) {
     const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
     const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
     int i = 0, j = 0;
+    char tempBuffer[50];
     for(i = 0; i < 5; i++) {
         for(j = 0; j < 5; j++) {
-            printf("%d | %s | %s\n", i * 5 + j, majorColor[i], minorColor[i]);
+            snprintf(tempBuffer, sizeof(tempBuffer), "%2d | %s | %s\n", i * 5 + j, majorColor[i], minorColor[j]);
+            strncat(outputBuffer, tempBuffer, bufferSize - strlen(outputBuffer) - 1);
         }
     }
     return i * j;
 }
 
 int main() {
-    int result = printColorMap();
+    char buffer[1000] = {0};
+    int result = printColorMap(buffer, sizeof(buffer));
     assert(result == 25);
+    // Check alignment of numbers
+    assert(strncmp(buffer, " 0 | White | Blue", 17) == 0); // This should fail due to misalignment
+    printf("%s", buffer);
     printf("All is well (maybe!)\n");
     return 0;
 }
+
